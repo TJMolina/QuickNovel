e443ec43a1d415e705dba48cba4a471534516d1d
TJMolina <tobiasmolina160@gmail.com>
2026-07-09 16:22:33 -0300

    optimization 2

--- app/src/main/java/com/lagradost/quicknovel/util/DefaultImagesHeaders.kt
+++
@@ -1,6 +0,0 @@
-package com.lagradost.quicknovel.util
-
-object DefaultImagesHeaders  {
-    val useCloudflareKillerHeader = "useCloudflareKiller" to "true"
-    val useIgnore500Header = "useIgnore500" to "true"
-}

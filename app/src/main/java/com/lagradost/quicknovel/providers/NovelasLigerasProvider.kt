11f2ada64b9de56bbfdc1f4701e7d1651498e70c
TJMolina <tobiasmolina160@gmail.com>
2026-08-26 21:08:31 -0300

    NovelasLigerasProvider fix

--- app/src/main/java/com/lagradost/quicknovel/providers/NovelasLigerasProvider.kt
+++ app/src/main/java/com/lagradost/quicknovel/providers/NovelasLigerasProvider.kt
@@ -1,6 +1,7 @@
 package com.lagradost.quicknovel.providers
 
 import android.net.Uri
+import com.lagradost.quicknovel.ErrorLoadingException
 import com.lagradost.quicknovel.HeadMainPageResponse
 import com.lagradost.quicknovel.LoadResponse
 import com.lagradost.quicknovel.MainAPI
@@ -8,10 +9,13 @@ import com.lagradost.quicknovel.R
 import com.lagradost.quicknovel.SearchResponse
 import com.lagradost.quicknovel.fixUrl
 import com.lagradost.quicknovel.fixUrlNull
+import com.lagradost.quicknovel.network.WebViewResolver
 import com.lagradost.quicknovel.newChapterData
 import com.lagradost.quicknovel.newSearchResponse
 import com.lagradost.quicknovel.newStreamResponse
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 class NovelasLigerasProvider : MainAPI() {
     override val name = "Nova"
@@ -35,6 +39,24 @@ class NovelasLigerasProvider : MainAPI() {
         "En Proceso" to "16",
         "Pausado" to "17"
     )
+
+    private suspend fun bypassCloudflare(select: String, url: String): String{
+        val script = """
+                         (function() {
+                             var checkInterval = setInterval(function() {
+                                 var element = document.querySelector("$select");
+                                
+                                 if (element.innerText.trim().length > 0) {
+                                     clearInterval(checkInterval);
+                                     NativeAndroid.onElementFound(document.querySelector("body").outerHTML);
+                                 }
+                             }, 1000);
+                             setTimeout(function() { clearInterval(checkInterval); }, 30000);
+                         })();
+                     """.trimIndent()
+        return WebViewResolver(scriptToFinish = script, useOkhttp = false).resolveUsingWebView(url) ?: throw ErrorLoadingException("Can't bypass")
+    }
+
     override suspend fun loadMainPage(
         page: Int,
         mainCategory: String?,
@@ -42,7 +64,10 @@ class NovelasLigerasProvider : MainAPI() {
         tag: String?
     ): HeadMainPageResponse {
         val url = "$mainUrl/index.php/lista-de-novela-ligera-novela-web/page/$page/?orderby=$orderBy${if(mainCategory.isNullOrEmpty())"" else "&ixwpst[pa_estado][]=$mainCategory"}&wps-title=1&wps-excerpt=1&wps-content=1&wps-categories=1&wps-attributes=1&wps-tags=1&wps-sku=1&ixwpsf[taxonomy][product_cat][show]=set&ixwpsf[taxonomy][product_cat][multiple]=0&ixwpsf[taxonomy][product_cat][filter]=1&ixwpsf[taxonomy][pa_estado][show]=set&ixwpsf[taxonomy][pa_estado][multiple]=0&ixwpsf[taxonomy][pa_estado][filter]=1&ixwpsf[taxonomy][pa_estado][op]=or&ixwpsf[taxonomy][pa_tipo][show]=set&ixwpsf[taxonomy][pa_tipo][multiple]=0&ixwpsf[taxonomy][pa_tipo][filter]=1&ixwpsf[taxonomy][pa_tipo][op]=or&ixwpsf[taxonomy][pa_pais][show]=set&ixwpsf[taxonomy][pa_pais][multiple]=0&ixwpsf[taxonomy][pa_pais][filter]=1&ixwpsf[taxonomy][pa_pais][op]=or"
-        val document = app.get(url).document
+        var document = app.get(url).document
+        if(document.selectFirst("div#content > div.products > div > div > article") == null){
+            document = Jsoup.parse(bypassCloudflare("div#content > div.products > div > div > article", url))
+        }
         val returnValue =
             document.select("div#content > div.products > div > div > article").mapNotNull { card ->
                 val href = card.selectFirst("a")?.attr("href") ?: return@mapNotNull null
@@ -60,9 +85,14 @@ class NovelasLigerasProvider : MainAPI() {
     }
 
     override suspend fun load(url: String): LoadResponse {
-        val document = app.get(url).document
-        val infoDiv = document.select("div.summary.entry-summary")
-        val title = infoDiv.selectFirst("h1")?.text() ?: ""
+        var document: Document = app.get(url).document
+        var infoDiv: Elements = document.select("div.summary.entry-summary")
+        var title: String? = infoDiv.selectFirst("h1")?.text()
+        if(title == null){
+            document = Jsoup.parse(bypassCloudflare("div.summary.entry-summary", url))
+            infoDiv = document.select("div.summary.entry-summary")
+            title = infoDiv.selectFirst("h1")?.text() ?: throw ErrorLoadingException("No title.")
+        }
 
         val synopsis = infoDiv.select("div.woocommerce-product-details__short-description > p")
             .joinToString("\n\n") { it.text() }
@@ -112,9 +142,13 @@ class NovelasLigerasProvider : MainAPI() {
     }
 
     override suspend fun loadHtml(url: String): String? {
-        val document = app.get(url).document
+        var document = app.get(url).document
+        val elementRequired = "div.wpb_text_column.wpb_content_element > div"
+        if(document.selectFirst(elementRequired) == null){
+            document = Jsoup.parse(bypassCloudflare(elementRequired, url))
+        }
         val reader =
-            document.selectFirst("div.wpb_text_column.wpb_content_element > div") ?: return null
+            document.selectFirst(elementRequired) ?: return null
         reader.select("h1, h2, a.track-ad").remove()
         return reader.html()
     }
